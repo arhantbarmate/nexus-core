3a52226fde810efeab2259c149f298717e54047a
arhantbarmate <arhantbarmate@users.noreply.github.com>
2026-01-13 21:41:46 +0000

    deploy: 664b300bc20f3be2fc841f153547f4301655aac2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "404579863d5cf6836ae3bf842e43695d",
+"flutter_bootstrap.js": "1736622dae9d942d018c3c37a4d493d3",
 "version.json": "c549842658ab5308d5c867efbccf157f",
 "main.dart.js": "4df2c89d0df53d944d241c1699974d46",
 "tonconnect-manifest.json": "1c5b8d236222d8133613267e924265ad"};
